2b90503a0ca2c8e65d2f14f1358809784d5efff4
kapasrilakshmi027 <kapasrilakshmi27@gmail.com>
2025-07-21 21:16:27 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /, %, //): ")
+
+if operation == '+':
+    print( a + b)
+elif operation == '-':
+    print( a - b)
+elif operation == '*':
+    print( a * b)
+elif operation == '/':
+    print( a / b)
+elif operation == '%':
+    print( a % b)
+elif operation == '//':
+    print( a // b)
+else:
+    print("Invalid operator")
