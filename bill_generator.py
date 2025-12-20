d7ba0444dedf045ddd79382f3db16262161b9804
kapasrilakshmi027 <kapasrilakshmi27@gmail.com>
2025-12-20 19:57:06 +0530

    Add files via upload
    A beginner-friendly Python bill generator that calculates the total amount based on item prices using variables and basic arithmetic.

---
+++ bill_generator.py
@@ -0,0 +1,12 @@
+item_1=float(input("Enter price of cocolate: "))
+item_2=float(input("Enter price of ice-cream: "))
+item_3=float(input("Enter price of cool drink: "))
+
+total_price=item_1+item_2+item_3
+
+print("\n-----Billing items-----")
+print("Chocolate price:",item_1)
+print("Ice-cream price:",item_2)
+print("Cool drink price:",item_3)
+print("Total price:",total_price)
+
