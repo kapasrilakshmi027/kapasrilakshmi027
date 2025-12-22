b6e44012c5a9537010f46842d80b450a2b987c0d
kapasrilakshmi027 <kapasrilakshmi27@gmail.com>
2025-12-22 21:45:59 +0530

    Add files via upload
    This project is a beginner-friendly Python application that simulates a basic login system. It verifies a username and password using if-else control flow logic and displays appropriate login messages.

---
+++ simple-login-system.py
@@ -0,0 +1,10 @@
+user_name="srii"
+user_password="1430709"
+
+username=input("Enter username: ")
+password=input("Enter password: ")
+
+if username==user_name and password==user_password:
+    print("\nLogin Successfull! welcome admin...")
+else:
+    print("\nLogin Failed! invalid username and password")
